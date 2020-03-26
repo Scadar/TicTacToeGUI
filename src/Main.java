b65f21c02b43f7cb808837484aa6c1450ead994d
Scadar <teghtml@yandex.ru>
2020-03-26 13:26:35 +0300

    Update .gitignore

--- src/Main.java
+++ src/Main.java
@@ -2,6 +2,7 @@ import game.Game;
 
 public class Main {
     public static void main(String[] args) {
+        System.out.println("Hello");
         Game game = new Game();
         game.initGame();
     }
